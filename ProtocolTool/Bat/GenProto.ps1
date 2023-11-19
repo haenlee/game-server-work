9ef6484298dd01e4c61d08816159b39861f5c614
haenlee <85219306+haenlee@users.noreply.github.com>
2023-11-19 18:19:25 +0900

    feat: gRPC 적용

---
+++ ProtocolTool/Bat/GenProto.ps1
@@ -0,0 +1,11 @@
+﻿$protoc = "..\..\Externals\protoc-3.18.0-win64\bin\protoc.exe"
+$csharp_out  = "../../GameServer/Proto"
+$proto_dir = "../Proto"
+
+$files = Get-ChildItem -Path $proto_dir
+foreach($file in $files) {
+	write-host $file
+	& $protoc --csharp_out=$csharp_out --proto_path=$proto_dir $file
+}
+
+
--- ServerCommon/Config.cs
+++ ServerCommon/Config.cs
@@ -5,6 +5,6 @@
         public static int PORT = 5010;
         public static int MAX_BACK_LOG = 100;
         public static int BUFFER_SIZE = 64 * 1024;
-        public static int HEADER_SIZE = 2;
+        public static int HEADER_SIZE = 4;
     }
 }
